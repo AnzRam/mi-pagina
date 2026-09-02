394c5b6451b17c2c698e1d5a820011439efdecbf
José Francisco Anzaldo Ramírez <josefranciscoanzaldoramirez@192.168.1.72>
2026-09-02 01:49:27 -0600

    v2 de la pagina

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-const boton = document.getElementById("boton");
+// Esperar a que cargue la página
+document.addEventListener("DOMContentLoaded", () => {
 
-boton.addEventListener("click", () => {
-    alert("¡Hola desde JavaScript!");
+    // Obtiene el nombre del archivo actual
+    let paginaActual = window.location.pathname.split("/").pop();
+
+    // Si estamos en la raíz (https://...)
+    if (paginaActual === "") {
+        paginaActual = "index.html";
+    }
+
+    // Recorre todos los enlaces del menú
+    document.querySelectorAll(".nav-link").forEach(link => {
+
+        if (link.dataset.page === paginaActual) {
+
+            // Resalta la opción actual
+            link.classList.add("active");
+
+            // Indica accesibilidad
+            link.setAttribute("aria-current", "page");
+
+            // Evita que el usuario vuelva a hacer clic
+            link.removeAttribute("href");
+
+        }
+
+    });
+
 });
